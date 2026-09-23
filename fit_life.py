21e494858c23ee54acb500415429e347770ebb95
Yuriy Samson <interestingworldfunny@gmail.com>
2026-09-23 15:04:48 +0500

    Правки

--- fit_life.py
+++ fit_life.py
@@ -1,29 +1,45 @@
 WATER_CONST = 30
+DECORATION_EQUAL = 38
+CONSTANT_DAILY_NORM = 1000
 
-print("=" * 38)
+print("=" * DECORATION_EQUAL)
 print("Добро пожаловать в наш сервис FitLife!")
-print("=" * 38)
+print("=" * DECORATION_EQUAL)
 
 user_name = input("Введите свое имя: ")
-user_age = int(input("Введите ваш возраст: "))
-user_weight = float(input("Введите свой вес в (кг): "))
-user_height = float(input("Введите свой рост в (метрах) 'например - 1.70': "))
+
+while True:
+    try:
+        user_age = int(
+            input("Введите количество полных лет, в целых числах: "))
+        break
+    except ValueError:
+        print("Вы ввели не число!")
+
+user_weight = float(
+    input("Введите свой вес в (кг): ")
+    .replace(",", "."))
+user_height = float(
+    input("Введите свой рост в (метрах) 'например - 1.70': ")
+    .replace(",", "."))
 
 
 def bmi_calculator():
-    """Индекс массы тела"""
+    """Индекс массы тела."""
     return user_weight / (user_height ** 2)
 
 
 def water_day_norm():
-    """Расчет суточной нормы воды"""
-    return (user_weight * WATER_CONST) / 1000
+    """Расчет суточной нормы воды."""
+    return (user_weight * WATER_CONST) / CONSTANT_DAILY_NORM
 
 
-print("=" * 40)
+print("=" * DECORATION_EQUAL)
 
-print(f"\nПривет {user_name}! Ваш результат: ")
-print(f"\nПользователь: {user_name}, Возраст - {user_age}")
-print(f"Ваш индекс массы тела(ИМТ): {round(bmi_calculator(), 1)}")
-print(f"Суточная норма воды: {round(water_day_norm(), 1)} л.")
-print("\nРасчет окончен. Будьте здоровы!")
+print(
+    f"Привет {user_name}! Ваш результат: \n"
+    f"Пользователь: {user_name}, Возраст - {user_age}\n"
+    f"Ваш индекс массы тела(ИМТ): {round(bmi_calculator(), 1)}\n"
+    f"Суточная норма воды: {round(water_day_norm(), 1)} л.\n"
+    f"Расчет окончен. Будьте здоровы!"
+)
